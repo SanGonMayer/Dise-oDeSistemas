d43947d569b339e1566428f8e5ed9ee26852a31b
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2024-04-01 00:16:33 -0300

    agrego codigo y diagrama

---
+++ ValidadorDeInscripciones/src/main/java/domain/Materia.java
@@ -0,0 +1,15 @@
+package domain;
+
+import java.util.List;
+
+public class Materia {
+    private List <Materia> correlativas;
+
+    public List<Materia> getCorrelativas() {
+        return this.correlativas;
+    }
+
+    public void soyMateria(){
+        return;
+    }
+}
